b5e4fe5a794ea00c7f9b5b11da4870a7ba56393b
Edouard Lopez <contact@edouard-lopez.com>
2020-12-31 17:46:02 +0100

    order variable by name for better discoverability

--- conf.d/ayu-dark.fish
+++ conf.d/ayu-dark.fish
@@ -1,22 +1,22 @@
 # AYU THEME: DARK
-set fish_color_normal         B3B1AD # ayu:common.fg        default color
+set fish_color_autosuggestion 4D5566 # ayu:common.ui        autosuggestions
 set fish_color_command        39BAE6 # ayu:syntax.tag       commands
-set fish_color_quote          C2D94C # ayu:syntax.string    quoted blocks of text
-set fish_color_redirection    FFEE99 # ayu:syntax.constant  IO redirections
+set fish_color_comment        626A73 # ayu:syntax.comment   code comments
+set fish_color_cwd            59C2FF # ayu:syntax.entity    current working directory in the default prompt
 set fish_color_end            F29668 # ayu:common.operator  process separators like ';' and '&'
 set fish_color_error          FF3333 # ayu:syntax.error     highlight potential errors
-set fish_color_param          B3B1AD # ayu:common.fg        regular command parameters
-set fish_color_comment        626A73 # ayu:syntax.comment   code comments
-set fish_color_match          F07178 # ayu:syntax.markup    highlight matching parenthesis
-set fish_color_selection      E6B450 # ayu:syntax.accent    when selecting text (in vi visual mode)
-set fish_color_search_match   --background E6B450 # ayu:syntax.accent    highlight history search matches and the selected pager item (must be a background)
-set fish_color_operator       E6B450 # ayu:syntax.accent    parameter expansion operators like '*' and '~'
 set fish_color_escape         95E6CB # ayu:syntax.regexp    highlight character escapes like '\n' and '\x70'
-set fish_color_cwd            59C2FF # ayu:syntax.entity    current working directory in the default prompt
-set fish_color_autosuggestion 4D5566 # ayu:common.ui        autosuggestions
+set fish_color_match          F07178 # ayu:syntax.markup    highlight matching parenthesis
+set fish_color_normal         B3B1AD # ayu:common.fg        default color
+set fish_color_operator       E6B450 # ayu:syntax.accent    parameter expansion operators like '*' and '~'
+set fish_color_param          B3B1AD # ayu:common.fg        regular command parameters
+set fish_color_quote          C2D94C # ayu:syntax.string    quoted blocks of text
+set fish_color_redirection    FFEE99 # ayu:syntax.constant  IO redirections
+set fish_color_search_match   --background E6B450 # ayu:syntax.accent    highlight history search matches and the selected pager item (must be a background)
+set fish_color_selection      E6B450 # ayu:syntax.accent    when selecting text (in vi visual mode)
 
 # color for fish default prompts item
-set fish_color_user           FF8F40 # ayu:syntax.keyword   current username in some of fish default prompts
+set fish_color_cancel         0A0E14 # ayu:common.bg        the '^C' indicator on a canceled command
 set fish_color_host           FFEE99 # ayu:syntax.constant  current host system in some of fish default prompts
 set fish_color_host_remote    FFEE99 # ayu:syntax.constant  current host system in some of fish default prompts, if fish is running remotely (via ssh or similar)
-set fish_color_cancel         0A0E14 # ayu:common.bg        the '^C' indicator on a canceled command
+set fish_color_user           FF8F40 # ayu:syntax.keyword   current username in some of fish default prompts
